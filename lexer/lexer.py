e929261f31bda8359d5eb5fd492d97c7f0d81578
[Francesca Parker (she/her)] SparkliTwizzl <github@sparklitwizzl.com>
2024-04-06 02:09:56 -0600

    supposedly i had it right before

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -16,8 +16,8 @@ class PetrichorScriptLexer(RegexLexer):
             (r'\\.', String.Escape), # escaped character
             (r'//.*\n', Comment.Single),
             (r',', Punctuation),
-            (r'[\{\}>>]', Operator),
-            # (r'>>', Operator),
+            (r'[\{\}]', Operator),
+            (r'>>', Operator),
             (r'([a-z][a-z0-9\-]*)(\s*)(:(?!:))', bygroups(Keyword.Reserved, Text, Operator)),
             (r'\[[a-z\-]+\]', Name.Variable),
             (r'"', String, 'string'),
