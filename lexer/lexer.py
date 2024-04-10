f943b8f669af8fd0af78be1ae268765b81e697ec
[Celeste Martin (they/any)] SparkliTwizzl <github@sparklitwizzl.com>
2024-04-10 03:12:02 -0600

    improve token detection

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -14,19 +14,19 @@ class PetrichorScriptLexer(RegexLexer):
         'root': [
             (r'\s+', Text), # whitespace
             (r'\\.', String.Escape), # escaped character
-            (r'//.*\n', Comment.Single),
-            (r',', Punctuation),
-            (r'[\{\}]', Operator),
-            (r'>>', Operator),
-            (r'([a-z][a-z0-9\-]*)(\s*)(:)', bygroups(Keyword.Reserved, Text, Operator)),
-            (r'\[[a-z\-]+\]', Name.Variable),
-            (r'"', String, 'string'),
-            (r'[0-9]+', Number),
+            (r'//.*\n', Comment.Single), # line comment
+            (r',', Punctuation), # comma
+            (r'[\{\}]', Operator), # token body boundaries
+            (r'(?<!>)>>(?!>)', Operator), # text shortcut template divider operator
+            (r'(^\s*[a-z]+(?:-[a-z0-9]+)*(?:-[a-z]+)?)(\s*)(:)', bygroups(Keyword.Reserved, Text, Operator)), # token name, whitespacae, token divider
+            (r'\[[a-z\-]+\]', Name.Variable), # field
+            (r'"', String, 'string'), # string start
+            (r'\b(-)?[0-9.]+\b', Number),
             (r'.', Text),
         ],
         'string': [
             (r'\\.', String.Escape),
-            (r'"', String, '#pop'),
+            (r'"', String, '#pop'), # string end
             (r'[^\\"]+', String),
         ]
     }
